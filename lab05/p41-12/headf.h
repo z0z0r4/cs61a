673852f4ae96add3cace736ccce3aa88af0cf108
z0z0r4 <z0z0r4@outlook.com>
2025-12-19 23:05:32 +0800

    del garbage

--- lab05/p41-12/headf.h
+++
@@ -1,5 +0,0 @@
-int find_factorial(int n);
-
-double solve_q1();
-
-int solve_q2();
--- lab05/p41-12/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "headf.h"
-
-int main() {
-    std::cout << solve_q1()  << " " << solve_q2();
-    return 0;
-}
