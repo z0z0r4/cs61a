98809682fb978845d814b07a1287417b3777d51d
z0z0r4 <z0z0r4@outlook.com>
2026-02-05 04:44:28 +0800

    lab08

---
+++ lab08/lab08.scm
@@ -0,0 +1,22 @@
+(define (over-or-under num1 num2) (cond 
+                                   ((> num1 num2) 1)
+                                   ((< num1 num2) -1)
+                                   (else 0)))
+
+(define (make-adder num) (lambda(inc) (+ num inc)))
+
+(define (composed f g) (lambda (x) (f (g x))))
+
+(define (repeat f n) (lambda(x) (if (> n 0) (f ((repeat f (- n 1)) x)) x)))
+
+(define (max a b)
+  (if (> a b)
+      a
+      b))
+
+(define (min a b)
+  (if (> a b)
+      b
+      a))
+
+(define (gcd a b) (if (zero? (modulo (max a b) (min a b))) (min a b) (gcd (modulo (max a b) (min a b)) (min a b))))
